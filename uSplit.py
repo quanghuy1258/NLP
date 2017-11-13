564dbfdd89105b9950ca3ad8b431785b2362c768
quanghuy1258 <quanghuy1258@gmail.com>
2017-11-13 20:38:16 +0700

    Create uSplit.py

---
+++ uSplit.py
@@ -0,0 +1,18 @@
+delim = []
+
+#Get the list of numeric and special characters
+for i in range(65):
+    delim.append(chr(i))
+for i in range(91,97):
+    delim.append(chr(i))
+for i in range(123,127):
+    delim.append(chr(i))
+
+#Split unicode strings with multiple delimiters
+def uSplit(uString,delim):
+    uString = list(uString)
+    for i in range(len(uString)):
+        if uString[i] in delim:
+            uString[i] = ' '
+    uString = ''.join(uString)
+    return uString.split()
